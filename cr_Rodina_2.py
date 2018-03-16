1d88c3948cbb6b7a530627a3e41a5bdaf713c5ad
nataschkarodina <36786299+nataschkarodina@users.noreply.github.com>
2018-03-16 23:07:24 +0300

    Delete cr_Rodina_2.py

--- cr_Rodina_2.py
+++
@@ -1,6 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("temparature_C", type=float,
-                    help="Temperature in Cel")
-args = parser.parse_args()
-answer = temperature_C**2
